c4c3fdf18888cad9d13ddae0dc255e373c7368d0
noctis_li <noctis_li@asus.com>
2025-01-20 23:31:25 +0800

    hw2 2025-01-20
    Signed-off-by: noctis_li <noctis_li@asus.com>

--- test_trigger.py
+++ test_trigger.py
@@ -1,5 +1,7 @@
 from student_assignment import *
 
 quest1 = "2024年台灣1月紀念日有哪些?"
+quest2 = "2024年台灣10月紀念日有哪些?"
 
-print("Trigger Return:", generate_hw01(question=quest1))
+print("Trigger HW1 Return:", generate_hw01(question=quest1))
+# print("Trigger HW2 Return:", generate_hw02(question=quest2))
