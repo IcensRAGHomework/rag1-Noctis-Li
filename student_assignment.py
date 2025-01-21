d60b5d6191665af58c3611b9d03b49f386d90a5a
noctis_li <noctis_li@asus.com>
2025-01-21 17:21:33 +0800

    hw3 2025-01-21
    Signed-off-by: noctis_li <noctis_li@asus.com>

--- student_assignment.py
+++ student_assignment.py
@@ -11,6 +11,9 @@ from langchain.agents import create_openai_functions_agent, AgentExecutor
 from langchain.prompts import PromptTemplate, MessagesPlaceholder, ChatPromptTemplate
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.tools import BaseTool, StructuredTool, tool
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
@@ -91,9 +94,64 @@ def generate_hw02(question):
     agent_executor = AgentExecutor(agent=agent, tools=tool, verbose=False)
     response = agent_executor.invoke({"input": question})
     return regeneration_json(source=response["output"])
-    
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
 def generate_hw03(question2, question3):
-    pass
+    tool = [get_holiday]
+    base_json = """
+{{
+    "Result": [
+        {{
+            "date": "2024-10-10",
+            "name": "國慶日"
+        }}
+    ]
+}}"""
+    base_json2 = """
+{{
+    "Result": 
+        {{
+            "add": true,
+            "reason": "蔣中正誕辰紀念日並未包含在十月的節日清單中。目前十月的現有節日包括國慶日、重陽節、華僑節、台灣光復節和萬聖節。因此，如果該日被認定為節日，應該將其新增至清單中。"
+        }}
+}}"""
+    prompt = ChatPromptTemplate([
+        ("system", """
+你是一個查詢工具,使用json格式返回用戶需求内容。
+返回内容務必完整準確且僅返回用戶所需的内容。
+
+回答範例：
+{template}"""),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ("user", "{input}"),
+        ("ai", "{ai_response}"),
+    ])
+    agent = create_openai_functions_agent(llm, tools=tool, prompt=prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tool, verbose=False)
+    agent_with_chat_history = RunnableWithMessageHistory(
+        agent_executor,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+    )
+
+    response = agent_with_chat_history.invoke(
+        {"template": base_json, "input":question2, "ai_response": generate_hw02(question2)},
+        config={"configurable": {"session_id": "<foo>"}},
+    )
+    print("invoke 1:\n", response["output"])
+    response = agent_with_chat_history.invoke(
+        {"template": base_json2, "input":question3, "ai_response": "{output}"},
+        config={"configurable": {"session_id": "<foo>"}},
+    )
+    print("invoke 2:\n", response["output"])
+    return regeneration_json(response["output"])
     
 def generate_hw04(question):
     pass
--- test_trigger.py
+++ test_trigger.py
@@ -2,6 +2,8 @@ from student_assignment import *
 
 quest1 = "2024年台灣1月紀念日有哪些?"
 quest2 = "2024年台灣10月紀念日有哪些?"
+quest3 = "根據先前的節日清單，這個節日{\"date\": \"10-31\", \"name\": \"蔣公誕辰紀念日\"}是否有在該月份清單？"
 
-print("Trigger HW1 Return:", generate_hw01(question=quest1))
-# print("Trigger HW2 Return:", generate_hw02(question=quest2))
+# # print("Trigger HW1 Return:\n", generate_hw01(question=quest1))
+# print("Trigger HW2 Return:\n", generate_hw02(question=quest2))
+print("Trigger HW3 Return:\n", generate_hw03(question2=quest2, question3=quest3))
